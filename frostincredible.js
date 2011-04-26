9916cb03ab20e1e69a25e027c4363df7a989729f
Kyle Frost <fsufrosty@gmail.com>
2011-04-26 17:40:11 -0400

    .js for frostincredibe

---
+++ frostincredible.js
@@ -0,0 +1,14 @@
+{
+    "version": "1",
+    "homepage": "http://fsufrosty.co.cc",
+    "roms":
+    [
+    {
+        "name": "FrostBlur v0.02 Alpha",
+        "summary": "Alpha",
+        "device": "inc",
+        "screenshot": "https://github.com/fsufrosty/FrostBlur-Repo/raw/master/FrostIncredible.png",
+        "url": "https://github.com/fsufrosty/FrostBlur-Repo/blob/master/FrostBlur_v002_frostincredible.zip?raw=true"
+    }
+    ]
+}
