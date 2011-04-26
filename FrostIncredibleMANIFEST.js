d2a656d658386dc49a249bbd358a1626670f8346
Kyle Frost <fsufrosty@gmail.com>
2011-04-26 17:44:55 -0400

    manifest for rom manager

---
+++ FrostIncredibleMANIFEST.js
@@ -0,0 +1,11 @@
+{
+    "developer": "FrostIncredible",
+    "free": "true",
+    "id": "frostincredible",
+    "icon": "https://github.com/fsufrosty/FrostBlur-Repo/raw/master/FrostIncredible.png",
+    "manifest": "https://github.com/fsufrosty/FrostBlur-Repo/raw/master/frostincredible.js",
+    "roms": {
+        "inc": "true"
+    },
+    "summary": "ROMs by FrostIncredible"
+}
