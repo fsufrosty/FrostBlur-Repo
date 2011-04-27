9bf3e3d95962f8bf4bbe33a3233387f88f5ca51f
Kyle Frost <fsufrosty@gmail.com>
2011-04-26 21:58:51 -0400

    changed to include all screenshots

--- frostincredible.js
+++ frostincredible.js
@@ -7,9 +7,12 @@
         "name": "FrostBlur v0.03 Alpha",
         "summary": "Alpha",
         "device": "inc",
-        "screenshot": "https://github.com/fsufrosty/FrostBlur-Repo/raw/master/Screenshot1.jpg",
-	"screenshot": "https://github.com/fsufrosty/FrostBlur-Repo/raw/master/Screenshot2.jpg",
-	"screenshot": "https://github.com/fsufrosty/FrostBlur-Repo/raw/master/Screenshot3.jpg",
+	"screenshots":
+[
+"https://github.com/fsufrosty/FrostBlur-Repo/raw/master/Screenshot1.jpg",
+"https://github.com/fsufrosty/FrostBlur-Repo/raw/master/Screenshot2.jpg",
+"https://github.com/fsufrosty/FrostBlur-Repo/raw/master/Screenshot3.jpg"
+],
         "url": "https://github.com/fsufrosty/FrostBlur-Repo/raw/master/FrostBlur-v003-frostincredible.zip"
     }
     ]
